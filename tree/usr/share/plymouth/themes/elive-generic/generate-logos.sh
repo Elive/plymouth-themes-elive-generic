daf487f02753c720b7ffead2a088714ece452de8
Thanatermesis <thanatermesis@gmail.com>
2014-09-18 15:45:33 +0200

    alignment of logo

--- tree/usr/share/plymouth/themes/elive-generic/generate-logos.sh
+++ tree/usr/share/plymouth/themes/elive-generic/generate-logos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsvg --format png --height 105 --width 80 plymouth-elive-logo.svg elive_logo.png
+rsvg --format png --height 105 --width 88 plymouth-elive-logo.svg elive_logo.png
 
 convert -colors 16 +dither elive_logo.png elive_logo16.png
